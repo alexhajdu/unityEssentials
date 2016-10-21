07c0c9a4034ac18c7302c8de50dd0e495d802430
Alex Hajdu <alex.hajdu@inloop.eu>
2016-10-21 10:57:05 +0200

    - Adde ScriptableObject utils + demo SO

---
+++ Scriptable Objects/Demo_SO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+    public class Demo_SO : ScriptableObject
+    {
+        public int foo;
+
+        [MenuItem ( "Alex/Create/SO" )]
+        public static void CreateAsset ( )
+        {
+            ScriptableObjectUtility.CreateAsset<Demo_SO> ( );
+        }
+    }
